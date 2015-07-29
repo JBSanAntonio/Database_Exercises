8f51b5af77470855363ee3580106881dfcb5737f
Julie Buser <jbuser2013@gmail.com>
2015-07-29 12:40:20 -0500

    second step

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,15 +4,3 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT *
 WHERE last_name like 'E%';
-
-SELECT *
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%12-31%';
-
-SELECT *
-FROM employees
-WHERE last_name like '%q%';
