b75a5d60e64e4104264d695bcc39069895152d3a
Julie Buser <jbuser2013@gmail.com>
2015-07-29 14:46:55 -0500

    step 6 added

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -14,7 +14,8 @@ ORDER BY emp_no DESC;
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC; 
 
 SELECT *
 FROM employees
