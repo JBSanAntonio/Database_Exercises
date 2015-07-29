cabba87b96ea00d431a46f0086c678ae7deb1620
Julie Buser <jbuser2013@gmail.com>
2015-07-29 14:59:39 -0500

    revised to add concat and order by alias

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,17 +1,17 @@
-SELECT first_name, last_name AS full_name
+SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE (first_name = 'Irena'
 	OR first_name = 'Vidya'
 	OR first_name = 'Maya')
 	AND gender = 'm'
-ORDER BY last_name DESC, first_name DESC;
+ORDER BY full_name, first_name DESC;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
 
-SELECT *
+SELECT hire_date AS 'when we hired this rockstar', concat(first_name, ' ', last_name) AS full_name, birth_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25'
