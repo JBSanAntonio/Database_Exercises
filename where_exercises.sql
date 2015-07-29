cad9de660757c46fb9acfaa7436532f9d3ad3c08
Julie Buser <jbuser2013@gmail.com>
2015-07-29 13:40:33 -0500

    updated where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,9 @@
-SELECT *
+SELECT concat(first_name, ‘ ‘, last_name) AS full_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT *
-WHERE last_name like 'E%';
+SELECT concat(first_name, ‘ ‘, last_name) AS full_name
+WHERE last_name LIKE 'E%';
 
 SELECT *
 FROM employees
@@ -11,8 +11,9 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT *
 FROM employees
-WHERE birth_date LIKE '%12-31%';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE last_name like '%q%';
+WHERE last_name LIKE '%q%';
