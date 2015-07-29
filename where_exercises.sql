f8c39f7eecff2e209a850feadcc236492090a959
Julie Buser <jbuser2013@gmail.com>
2015-07-29 14:07:57 -0500

    step 4 added

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,13 +11,10 @@ WHERE last_name LIKE 'e%e';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
-SELECT *
-FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+	AND last_name NOT LIKE '%qu%';
